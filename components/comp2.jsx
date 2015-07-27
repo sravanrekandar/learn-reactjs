048933bb3cb2bda8075319749ae617e0fb008531
Srekans <sravanrekandar@gmail.com>
2015-07-28 00:02:07 +0530

    with webpack build

--- components/comp2.jsx
+++
@@ -1,6 +0,0 @@
-var Comp = React.createClass({
-	render: function(){
-		return <h1>React</h1>
-	}
-});
-module.exports = Comp;
--- components/examples/static-text.jsx
+++
@@ -1,9 +0,0 @@
-/** @filename examples/static-text.jsx
-    @author SRAVAN
- */
-var StaticText = React.createClass({
-	render: function(){
-		return <h3>Hello React!</h3>
-	}
-});
-module.exports = StaticText;
